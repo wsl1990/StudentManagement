13667d8f5adb1696e34adb9a2ad47cacc4fad400
applemacwsl <cool-wsl@163.com>
2019-12-07 22:01:45 +0800

    middleware

--- StudentManagement/Startup.cs
+++ StudentManagement/Startup.cs
@@ -5,13 +5,21 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace StudentManagement
 {
     public class Startup
     {
+        private readonly IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -26,15 +34,29 @@ namespace StudentManagement
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseStaticFiles();
 
             app.UseRouting();
 
-            app.UseEndpoints(endpoints =>
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.MapGet("/", async context =>
+            //    {
+            //        await context.Response.WriteAsync("Hello World!");
+            //    });
+            //});
+            //app.Use(async (contxt, next) =>
+            //{
+            //    await contxt.Response.WriteAsync("First page");
+            //    loger.LogInformation("M1: input request");
+            //    await next();
+            //    loger.LogInformation("M1: output request");
+            //});
+
+            app.Run(async (context) =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                string configValue = _configuration["MyKey"];
+                await context.Response.WriteAsync(configValue);
             });
         }
     }
